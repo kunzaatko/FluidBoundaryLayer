313d1c990957ed34ce413d0df1517587925174a9
Martin Kunz <martinkunz@email.cz>
2021-05-08 12:24:38 +0200

    edit: add print true value and variable number of steps

--- test/test_28.c
+++ test/test_28.c
@@ -1,6 +1,6 @@
+#include "test_28.h"
 #include "../src/28.h"
 #include "vendor/unity.h"
-#include "test_28.h"
 
 void setUp(void) {}
 
@@ -20,8 +20,12 @@ static void test_euler_usint(void) {
   z[2] = 1;
   float *zout;
   zout = vector(1, 2);
+  int nsteps = 10;
   // true value is ~ 14.828
-  usint(z, 2, 500, 0, 1, zout, *derivs_euler);
+  usint(z, 2, nsteps, 0, 1, zout, *derivs_euler);
+  printf("Value of y(1) = e^{-3} + 2e^{2} estimated from %d steps is %.3f "
+         "(true value 14.828)\n",
+         nsteps, zout[1]);
   TEST_ASSERT_TRUE(zout[1] < 16);
   TEST_ASSERT_TRUE(zout[1] > 14);
   free_vector(z, 1, 2);
