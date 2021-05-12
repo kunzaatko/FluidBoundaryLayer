566b287ab6771553d0938f5e62cd683eeb764dd9
Martin Kunz <martinkunz@email.cz>
2021-05-12 16:02:48 +0200

    add: ploting script in julia

---
+++ make_plot.jl
@@ -0,0 +1,44 @@
+if "-h" in ARGS || "--help" in ARGS
+    print("""
+usage:
+    julia -i make_plot.jl {{csv_filename}} {{lambda_value}} {{tikz_file_for_saving}}
+
+    The tikz file is optional.
+""")
+end
+
+using CSV, DataFrames, Plots, LaTeXStrings
+pyplot()
+
+c = DataFrame(CSV.File(ARGS[1]))
+lambda = ARGS[2]
+
+plot(
+    Vector(c[:, 1]),
+    Vector(c[:, 2]);
+    label=L"y(x)",
+    title=L"\lambda = %$lambda",
+    leg=:topleft,
+)
+
+plot!(Vector(c[:, 1]), Vector(c[:, 3]); label=L"y'(x)")
+
+plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)", show=true)
+
+if length(ARGS) == 3
+    tex_output_file = ARGS[3]
+    pgfplotsx()
+
+    plot(
+        Vector(c[:, 1]),
+        Vector(c[:, 2]);
+        label=L"y(x)",
+        title=L"\lambda = %$lambda",
+        leg=:topleft,
+    )
+
+    plot!(Vector(c[:, 1]), Vector(c[:, 3]); label=L"y'(x)")
+
+    plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)")
+    savefig(tex_output_file * ".tikz")
+end
