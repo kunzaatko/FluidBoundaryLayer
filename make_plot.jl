88c832ab8f6428a0cd5e16adb21692a50c78cd72
Martin Kunz <martinkunz@email.cz>
2021-07-15 23:58:30 +0200

    edit: making it a function to break from it after help

--- make_plot.jl
+++ make_plot.jl
@@ -1,33 +1,19 @@
-if "-h" in ARGS || "--help" in ARGS
-    print("""
-usage:
-    julia -i make_plot.jl {{csv_filename}} {{lambda_value}} {{file_for_saving (in svg format)}}
-
-    The file for save is optional.
-""")
-end
-
 using CSV, DataFrames, Plots, LaTeXStrings
-pyplot()
 
-c = DataFrame(CSV.File(ARGS[1]))
-lambda = ARGS[2]
+function make_plots()
+    if "-h" in ARGS || "--help" in ARGS
+        print("""
+    usage:
+        julia -i make_plot.jl {{csv_filename}} {{lambda_value}} {{file_for_saving (in svg format)}}
 
-plot(
-    Vector(c[:, 1]),
-    Vector(c[:, 2]);
-    label=L"y(x)",
-    title=L"\lambda = %$lambda",
-    leg=:topleft,
-)
+        The file for save is optional.
+    """)
+        return # this is for the possibility to quit out of the include before running other stuff when wanting only help
+    end
 
-plot!(Vector(c[:, 1]), Vector(c[:, 3]); label=L"y'(x)")
-
-plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)", show=true)
-
-if length(ARGS) == 3
-    tex_output_file = ARGS[3]
-    pgfplotsx()
+    pyplot()
+    c = DataFrame(CSV.File(ARGS[1]))
+    lambda = ARGS[2]
 
     plot(
         Vector(c[:, 1]),
@@ -39,6 +25,25 @@ if length(ARGS) == 3
 
     plot!(Vector(c[:, 1]), Vector(c[:, 3]); label=L"y'(x)")
 
-    plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)")
-    savefig(tex_output_file * ".svg")
+    plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)", show=true)
+
+    if length(ARGS) == 3
+        tex_output_file = ARGS[3]
+        pgfplotsx()
+
+        plot(
+            Vector(c[:, 1]),
+            Vector(c[:, 2]);
+            label=L"y(x)",
+            title=L"\lambda = %$lambda",
+            leg=:topleft,
+        )
+
+        plot!(Vector(c[:, 1]), Vector(c[:, 3]); label=L"y'(x)")
+
+        plot!(Vector(c[:, 1]), Vector(c[:, 4]); label=L"y''(x)")
+        savefig(tex_output_file * ".svg")
+    end
+    return
 end
+make_plots()
